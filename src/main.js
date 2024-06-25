6e5d2a08a99a7956395111a48344a270ba489027
Zaklekta <zaklekta@gmail.com>
2024-06-25 13:22:01 +0000

    removed axcesive console.log

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ const perPage = 15;
 refs.formElem.addEventListener('submit', async e => {
   e.preventDefault();
   hideLoadMoreBtn();
+  currentPage = 1;
   refs.pixabayListElem.innerHTML = '';
   searchQuery = e.target.elements.query.value.trim();
   if (searchQuery !== '') {
